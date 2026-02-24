fb18f1bbfcf8f6ce142a74ecb376f18364b91891
OLAPROD-sy <allagbesylvanus1@.com>
2026-02-24 21:47:13 +0100

    Version initiale NMS Planning

--- admin/add_users.php
+++ admin/add_users.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../includes/csrf.php';
+//require_once __DIR__ . '/../includes/csrf.php';
 //require_once __DIR__ . '/../includes/auth_check.php';
 require_once __DIR__ . '/../config/database.php';
 
