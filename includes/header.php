43b3aaa7d50460af1e48fc119915ac4bee6c1331
OLAPROD-sy <allagbesylvanus1@.com>
2026-03-04 17:30:12 +0100

    Version initiale NMS Planning

--- includes/header.php
+++ includes/header.php
@@ -42,6 +42,9 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NMS Planning</title>
+    <link rel="icon" type="image/svg+xml" href="/assets/images/favicon.svg">
+    
+    <link rel="alternate icon" href="/assets/images/logo_nms.png">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;600;800&display=swap" rel="stylesheet">
     <style>
     :root {
